1530448b4f332636c05f2b843c3ef68571bd969c
Nekofoxmiu <ioio920430@gmail.com>
2024-04-05 06:58:39 +0800

    基本完善拖曳和修改邏輯

--- content.js
+++ content.js
@@ -1,13 +1,25 @@
 'use strict';
+console.log('yt-paj content.js injected');
+
 (async () => {
-    console.log('yt-paj content.js injected');
-
-    // 定義拖動項目和播放列表項目
-    //let dragItem;
-    //let dragImage;
-    //let playlistItems = [];
-    //let state = [];
-
+    let dataClassModule;
+    let playlistToolModule;
+    let mouseEventHandlerModule;
+    try {
+        dataClassModule = await import('./lib/dataclass.js');
+        playlistToolModule = await import('./lib/playlistTool.js');
+        mouseEventHandlerModule = await import('./lib/mouseEventHandler.js');
+    }
+    catch (error) {
+        console.error('Module loading failed:', error);
+    }
+    const getandUpdatePlaylistState = playlistToolModule.getandUpdatePlaylistState;
+    const TimeSlot = dataClassModule.TimeSlot;
+    const PlaylistItem = dataClassModule.PlaylistItem;
+    const PlaylistState = dataClassModule.PlaylistState;
+    const PlaylistTimeManager = playlistToolModule.PlaylistTimeManager;
+    const MouseEventHandler = mouseEventHandlerModule.MouseEventHandler;
+    
     // 建立播放列表容器
     const playlistContainer = document.createElement('div');
     playlistContainer.id = 'playlist-container';
@@ -22,196 +34,22 @@
     const ul = document.createElement('ul');
     ul.id = 'ytj-playlist-items';
 
-
-    let dataClassModule;
-    let playlistToolModule;
-    try {
-        dataClassModule = await import('./lib/dataclass.js');
-        playlistToolModule = await import('./lib/playlistTool.js');
-    }
-    catch (error) {
-        console.error('Module loading failed:', error);
-    }
-    const TimeSlot = dataClassModule.TimeSlot;
-    const PlaylistItem = dataClassModule.PlaylistItem;
-    const PlaylistState = dataClassModule.PlaylistState;
-    const PlaylistCheckTool = playlistToolModule.PlaylistCheckTool;
-    const PlaylistTimeManager = playlistToolModule.PlaylistTimeManager;
-
     const playlistState = new PlaylistState();
-
-    class MouseEventHandler extends PlaylistCheckTool{
-        /**
-        * Create a playlist item.
-        * @param {HTMLElement} playlistContainer - The container element for the playlist.
-        * @param {PlaylistState} sharedState - The shared state object.
-        */
-        constructor(ul, playlistContainer, sharedState) {
-            super(playlistContainer, sharedState);
-            this.playlistContainer = playlistContainer;
-            this.sharedState = sharedState;
-            this.dragItem = null;
-            this.dragImage = null;
-            this.ul = ul;
-            this.waitCount = 3;
-            // 其他需要的初始化代碼...
-        }
-
-        createDragImage = (dragItem, event) => {
-            const computedStyle = window.getComputedStyle(dragItem);
-            const computedWidth = computedStyle.width;
-            const computedHeight = computedStyle.height;
-
-            const dragImage = dragItem.cloneNode(true);
-            const dragHandle = dragImage.querySelector('.drag-handle');
-
-
-            dragItem.removeEventListener('dragstart', this.handleDragStart);
-            dragHandle.classList.remove('drag-handle');
-            dragHandle.classList.add('drag-handle-clicked');
-            dragImage.classList.remove('ytj-playlist-item');
-            dragImage.classList.add('display-dragging');
-            //handler偏移量
-            dragImage.style.position = 'absolute';
-            dragImage.style.top = `-${16}px`;
-            dragImage.style.left = `-${16}px`;
-            dragImage.style.width = computedWidth;
-            dragImage.style.height = computedHeight;
-            dragImage.style.zIndex = 1000;
-            //先隱藏避免出現在畫面上
-            dragImage.style.opcity = 0;
-
-            return dragImage;
-        };
-
-        // 定義獲取拖放位置的函數
-        getDragCrossElement = (ul, querySelector, y) => {
-            const draggableElements = [...ul.querySelectorAll(querySelector)];
-
-            return draggableElements.reduce((closest, child) => {
-                const box = child.getBoundingClientRect();
-                const offset = y - box.top - box.height * (1 / 2);
-
-                if (offset < 0 && offset > closest.offset) {
-                    return { offset: offset, element: child };
-                } else {
-                    return closest;
-                }
-            }, { offset: Number.NEGATIVE_INFINITY }).element;
-        };
-
-        /**
-        * 初始化拖曳過程。
-        * @param {HTMLElement} item - 要拖曳的播放列表項目。
-        * @param {MouseEvent} event - 觸發拖曳的事件對象。
-        */
-        handleDragStart = (event) => {
-            const dragHandle = event.target.closest('.drag-handle');
-            if (!dragHandle) return;
-
-            const playlistItem = dragHandle.closest('.ytj-playlist-item');
-            // 避免對非 playlist-item 元素進行操作
-            if (!playlistItem) {
-                return;
-            }
-            event.preventDefault();
-
-            this.dragItem = playlistItem; // 設置拖動項目的參考
-            // 創建拖曳縮略圖元素
-            this.dragImage = this.createDragImage(this.dragItem, event);
-            document.body.appendChild(this.dragImage);
-
-            // 設置拖曳縮略圖的位置 恢復顯示 以及綁定事件
-            this.dragImage.style.opcity = 1;
-            this.dragImage.style.transform = `translateX(${event.pageX}px) translateY(${event.pageY}px)`;
-            this.dragItem.classList.add('dragging');
-            document.body.style.cursor = 'grabbing';
-
-            // 添加移動和放開的事件監聽器
-            document.addEventListener('mousemove', this.handleDragging);
-            document.addEventListener('mouseup', this.handleDragEnd);
-        };
-
-        /**
-         * 處理拖動過程中的事件。
-         * @param {MouseEvent} event - 觸發拖動事件的事件對象。
-         */
-        handleDragging = (event) => {
-            if (!this.dragImage) {
-                console.error('拖曳圖像未找到。');
-                return;
-            }
-
-
-            requestAnimationFrame(() => {
-                if (!this.dragImage.style) {
-                    return;
-                }
-                //1000是為了移出畫面的偏移量
-                this.dragImage.style.transform = `translateX(${event.pageX}px) translateY(${event.pageY}px)`;
-
-
-                // 更新拖曳縮略圖的位置
-
-                this.waitCount--;
-                if (this.waitCount > 0) {
-                    return;
-                }
-                this.waitCount = 3;
-
-                const movingY = event.clientY;
-                const crossElement = this.getDragCrossElement(this.ul, '.ytj-playlist-item:not(.dragging)', movingY);
-
-                // 進行 DOM 操作前確保有改變再操作，避免不必要的性能消耗
-                if (crossElement == null) {
-                    this.ul.appendChild(this.dragItem);
-                } else {
-                    if (this.dragItem.nextElementSibling !== crossElement) {
-                        this.ul.insertBefore(this.dragItem, crossElement);
-                    }
-                }
-            });
-
-        };
-
-        // 定義處理拖動結束的函數
-        handleDragEnd = () => {
-            // 移除滑鼠事件的監聽器
-            document.removeEventListener('mousemove', this.handleDragging);
-            document.removeEventListener('mouseup', this.handleDragEnd);
-
-            if (this.dragImage) {
-                this.dragImage.remove(); // 移除拖曳縮略圖
-                this.dragImage = null;
-            }
-
-            this.dragItem.classList.remove('dragging');
-            document.body.style.cursor = 'default';
-            this.dragItem = null; // 清除拖動項目的參考
-            this.sharedState.playlistItems = Array.from(this.playlistContainer.querySelectorAll('.ytj-playlist-item'));
-            this.sharedState.state = this.getandUpdatePlaylistState(); // 更新播放列表狀態
-        }
-
-    }
-
-    const playlistCheckTool = new PlaylistCheckTool(playlistContainer, playlistState);
     const mouseEventHandler = new MouseEventHandler(ul, playlistContainer, playlistState);
     const playlistTimeManager = new PlaylistTimeManager(playlistContainer, playlistState);
 
+
     /**
      * 獲取當前視頻播放時間，並轉換為小時、分鐘和秒。
-     * @returns {?{hours: number, minutes: number, seconds: number, allseconds: number}} 包含時間信息的物件，或者如果沒有視頻元素則返回 null。
+     * @returns {?{TimeSlot}} 包含時間信息的物件，或者如果沒有視頻元素則返回 null。
      */
     const getCurrentVideoTime = () => {
         const video = document.querySelector('video');
         if (!video) return null;
 
-        const unparsedseconds = Math.floor(video.currentTime);
-        const hours = Math.floor(unparsedseconds / 3600);
-        const minutes = Math.floor((unparsedseconds % 3600) / 60);
-        const seconds = unparsedseconds % 60;
+        const videoTime = TimeSlot.fromTotalseconds(Math.floor(video.currentTime));
 
-        return { hours, minutes, seconds, allseconds: unparsedseconds };
+        return videoTime;
     };
 
     /**
@@ -228,50 +66,9 @@
 
         const itemText = document.createElement('div');
         itemText.classList.add(`ytj-playlist-item-text-${startOrEnd}`);
-        itemText.innerText = playlistTimeManager.formatTime(timeObj);
+        itemText.innerText = timeObj.toformatString();
+        itemText.setAttribute('timeat', timeObj.getTotalseconds().toString());
         itemText.contentEditable = false;
-
-        let originalText = itemText.innerText;
-
-        // 定義事件處理函數來處理不同的用戶互動
-        const enableEditMode = () => {
-            itemText.contentEditable = true;
-            itemText.focus();
-        };
-
-        const saveEdits = () => {
-            itemText.contentEditable = false;
-            playlistTimeManager.updateTimeText(itemText, originalText);
-            originalText = itemText.innerText;
-            playlistState.state = playlistCheckTool.getandUpdatePlaylistState();
-        };
-
-        const cancelEdits = () => {
-            itemText.innerText = originalText;
-            itemText.contentEditable = false;
-        };
-
-        // 事件監聽器
-        itemText.addEventListener('click', enableEditMode);
-        itemText.addEventListener('keydown', (event) => {
-            //阻止事件冒泡被YT播放器捕捉
-            event.stopPropagation();
-            if (event.key === 'Enter' || event.key === 'Escape') {
-                event.preventDefault(); // 只有在需要時阻止默認行為
-                if (event.key === 'Enter') {
-                    event.target.blur();
-                } else if (event.key === 'Escape') {
-                    event.target.blur();
-                    cancelEdits();
-                }
-            }
-        });
-        itemText.addEventListener('keyup', (event) => {
-            //阻止事件冒泡被YT播放器捕捉
-            event.stopPropagation();
-        });
-        itemText.addEventListener('blur', saveEdits);
-
         return itemText;
     };
 
@@ -302,10 +99,47 @@
 
             // 使用事件委派來處理所有子項目的 mousedown 事件
             ul.addEventListener('mousedown', event => {
-                const dragHandle = event.target.closest('.drag-handle');
+                const dragHandle = event.target.closest('.ytj-drag-handle');
                 if (dragHandle) {
                     mouseEventHandler.handleDragStart(event);
                 }
+            });
+
+            // 使用事件委派來處理所有子項目的點擊、編輯和保存邏輯
+            playlistContainer.addEventListener('click', (event) => {
+                const timeTextElement = event.target.closest('.ytj-playlist-item-text-start, .ytj-playlist-item-text-end');
+                if (!timeTextElement) return;
+
+                if (timeTextElement.contentEditable === 'true') return; // 如果已經是編輯模式，則不進行操作
+
+                // 啟用編輯模式
+                const originalText = timeTextElement.innerText;
+                const originalTime = Number(timeTextElement.getAttribute('timeat'));
+                timeTextElement.contentEditable = 'true';
+                timeTextElement.focus();
+
+                // 設置失去焦點事件處理
+                const onBlur = () => {
+                    timeTextElement.removeEventListener('blur', onBlur); // 移除本身的事件監聽器以避免重復註冊
+                    timeTextElement.contentEditable = 'false';
+                    playlistTimeManager.updateTimeText(timeTextElement, originalTime);
+                    playlistState.state = getandUpdatePlaylistState(playlistState);
+                };
+
+                // 設置鍵盤事件，以支持保存和取消
+                const onKeydown = (event) => {
+                    event.stopPropagation(); // 防止事件冒泡
+                    if (event.key === 'Enter') {
+                        event.preventDefault(); // 防止Enter鍵的默認行為
+                        timeTextElement.blur(); // 觸發失去焦點事件來保存
+                    } else if (event.key === 'Escape') {
+                        timeTextElement.innerText = originalText; // 恢復原始文本
+                        timeTextElement.blur(); // 取消編輯
+                    }
+                };
+
+                timeTextElement.addEventListener('keydown', onKeydown);
+                timeTextElement.addEventListener('blur', onBlur, { once: true }); // 使用{ once: true }確保事件僅被觸發一次
             });
 
             /**
@@ -338,7 +172,7 @@
                 newItem.classList.add('ytj-playlist-item');
 
                 const dragHandle = document.createElement('div');
-                dragHandle.classList.add('drag-handle');
+                dragHandle.classList.add('ytj-drag-handle');
                 dragHandle.draggable = true;
                 dragHandle.addEventListener('dragstart', mouseEventHandler.handleDragStart);
                 // 添加時間標籤，用於顯示和編輯開始和結束時間
@@ -360,7 +194,7 @@
                 playlistState.playlistItems.push(newItem);
                 ul.appendChild(newItem);
                 playlistContainer.appendChild(ul);
-                playlistState.state = playlistCheckTool.getandUpdatePlaylistState();
+                playlistState.state = getandUpdatePlaylistState(playlistState);
             }
             // 監聽添加到播放列表按鈕的點擊事件
             addToPlaylistButton.addEventListener('click', addToPlaylist);
